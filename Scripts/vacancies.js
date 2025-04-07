c38816cb56dcf361b86f296f2cd412a148ac45bc
Michael Surgeon <m.surgeon@craneware.com>
2025-04-07 02:31:41 +0100

    Add features to vacancies page

--- Scripts/vacancies.js
+++ Scripts/vacancies.js
@@ -1,32 +1,184 @@
-window.addEventListener("load", async (event) => {
-    const vacanciesList = document.getElementById("vacancies-list");
+let filteredData = []
+let currPageSize = 10;
 
-    let data = JSON.parse(localStorage.getItem("vacancies-data"));
+async function load(event) {
 
-    if(data === null || data.length == 0) {
-        console.log("Fetching Api Data");
-        data = await GetData();
-        localStorage.setItem("vacancies-data", JSON.stringify(data));  
+    const filters = getFilters();
+
+    url = getUrlFromSearchFilter(filters);
+
+    const data = await GetDataAsync(url);
+    
+    if(!data) {
+        alert("Request limit hit, (refresh after two seconds)")
+        return;
     }
 
-    const firstFive = data.splice(0, 5);
-    firstFive.forEach(job => {
-        createNewVacanciesCard(vacanciesList, job);
-    })
+    filteredData = getLocationFilteredData(filters, data)
+    if(filteredData.length < 10) {
+        document.getElementById("vacancies-next").style.display = "none";
+        document.getElementById("vacancies-back").style.display = "none";
+    } else {
+        document.getElementById("vacancies-next").style.display = "block";
+    }
 
-    setUpExpandableCard();
-});
+    await GenerateData(filteredData.slice(0, 10));
+}
 
-async function GetData() {
-    const url = "https://api.lmiforall.org.uk/api/v1/vacancies/search";
+function getUrlFromSearchFilter(filters) {
+    let url = "https://api.lmiforall.org.uk/api/v1/vacancies/search";
+    const searchFilter = filters.get("search");
+
+    if(searchFilter) {
+        const existingSearchChip = document.querySelector(".filter-buble[filter-chip-type='search']");
+        if(!existingSearchChip) {
+        createFilterChips(searchFilter, "search");
+        }
+
+        url = url + `?keywords=${searchFilter}`;
+    }
+
+    return url;
+}
+
+function getLocationFilteredData(filters, data) {
+    const locationFilter = filters.get("location");
     
+    if(locationFilter) {
+        const existingLocationChip = document.querySelector(".filter-buble[filter-chip-type='location']");
+        if(!existingLocationChip) {
+        createFilterChips(locationFilter, "location");
+        }
+
+        let filteredData = [];
+        data.forEach(job => {
+            if(job.location.location.includes(locationFilter)) {
+                filteredData.push(job);
+            }
+        })
+        return filteredData;
+    }
+    return data;
+}
+
+async function GetDataAsync(url) {
     try {
-        const response = await fetch(url);
-        const json = await response.json();  
-        return json; 
+        let response = await fetch(url);
+
+        if(response.status != 200)
+        {
+            console.log("Error - Status Code: " + response.status);
+            setTimeout(async function() 
+            { 
+                response = await fetch(url); 
+            }, 2000);
+            return;
+        }
+
+        if(response) {
+            const json = await response.json();  
+            return json; 
+        }
+
+        return null;
     } catch (error) {
         console.error('Error:', error);
     }
+}
+
+async function GenerateData(data) {
+    const cards = document.getElementsByClassName("vacancies-card");
+    while (cards.length > 0) {
+        cards[0].remove(); 
+    }    
+    
+    let spinnerIcon = document.getElementById("vacancies-spinner");
+    spinnerIcon.style.display = "block";
+
+
+    for (let job of data) {
+        let jobInformation = await GetDataAsync(`https://api.lmiforall.org.uk/api/v1/soc/search?q=${job.title}`);
+        
+        for (let info of jobInformation) {
+            for (let jt of info.add_titles) {
+                if (jt.toLowerCase() === job.title.toLowerCase()) {
+                    job.description = info.description;
+                    job.tasks = info.tasks;
+                }
+            }
+        }
+
+        createNewVacanciesCard(job);
+    }
+
+    spinnerIcon.style.display = "none";
+
+    setUpExpandableCard();
+}
+
+async function handleSearchSubmit(event) {
+    event.preventDefault();
+    removeFilterFromState("search");
+
+    let searchQuery = document.getElementById("vacancies-search-box").value;
+    let inputBox = document.getElementById("vacancies-search-box");
+
+    if (!searchQuery) {
+        inputBox.style.border = "2px solid red";
+        inputBox.placeholder = "You Must Enter A Value";
+        return;
+    }
+
+    inputBox.style.border = "1px solid black";
+    inputBox.placeholder = "Software Engineer";
+
+    let existingSearchChip = document.querySelector("[filter-chip-type='search']");
+    if (existingSearchChip) {
+        existingSearchChip.remove();
+    }
+
+    document.getElementById('vacancies-form').reset();
+    addFilterToState("search", searchQuery);
+    createFilterChips(searchQuery, "search");
+    load();
+}
+
+function openFilterOverlay(event) {
+    event.preventDefault();
+
+    document.body.style.overflow = "hidden";
+    document.getElementById("filters-overlay").style.display = "block";
+
+    let filtersLocation = document.getElementById("filters-location");
+
+    if (filteredData.length > 0 && filtersLocation) {
+        const locations = [...new Set(filteredData.map(d => d.location.location))]; 
+        locations.forEach(loc => {
+            let option = document.createElement("option");
+            option.value = loc;
+            option.innerHTML = loc;
+            filtersLocation.appendChild(option);
+        });
+    }
+}
+
+function closeFilterOverlay(event) {
+    event.preventDefault();
+
+    document.body.style.overflow = "visible";
+    document.getElementById("filters-overlay").style.display = "none";
+}
+
+function handleFilterSubmit(event) {
+    event.preventDefault();
+
+    const location = document.getElementById("filters-location").value;
+    addFilterToState("location", location);
+    createFilterChips(location, "location");
+
+    document.body.style.overflow = "visible";
+    document.getElementById("filters-overlay").style.display = "none";
+    load();
 }
 
 function setUpExpandableCard() {
@@ -38,7 +190,6 @@ function setUpExpandableCard() {
         const expandableContent = element.closest('.vacancies-card').querySelector('.vacancies-card-expandable');
         const showLessButton = element.closest('.vacancies-card').querySelector('.vacancies-card-expandable-show-less');
         const showMoreButton = element.closest('.vacancies-card').querySelector('.vacancies-card-expandable-show-more');
-        showMoreButton.scrollIntoView();
 
         expandableContent.style.display = "block";
         showLessButton.style.display = "block";
@@ -62,8 +213,9 @@ function setUpExpandableCard() {
     }));
 }
 
+function createNewVacanciesCard(data) {
+    const element = document.getElementById("vacancies-list");
 
-function createNewVacanciesCard(element, data) {
     let newCard = document.createElement("div");
     newCard.classList.add("vacancies-card");
 
@@ -71,6 +223,40 @@ function createNewVacanciesCard(element, data) {
     newJobTitle.innerHTML = data.title;
     newJobTitle.classList.add("vacancies-card-job-title");
     newCard.appendChild(newJobTitle);
+
+    let generalInformationDiv = document.createElement("div");
+    generalInformationDiv.classList.add("vacancies-card-general");
+
+    if(data.description && data.tasks){    
+        let description = document.createElement("h3");
+        description.innerHTML = "Description";
+
+        let generalInformationDesc = document.createElement("p");
+        generalInformationDesc.innerHTML = data.description;
+
+        let tasks = document.createElement("h3");
+        tasks.innerHTML = "Tasks";
+
+        let generalInformationTasks = document.createElement("p");
+        generalInformationTasks.innerHTML = data.tasks;
+
+        generalInformationDiv.appendChild(description)
+        generalInformationDiv.appendChild(generalInformationDesc);
+        generalInformationDiv.appendChild(tasks)
+        generalInformationDiv.appendChild(generalInformationTasks);
+    } 
+    else 
+    {
+        let general = document.createElement("h3");
+        general.innerHTML = "General Information";
+        generalInformationDiv.appendChild(general);
+
+        let generalInformationGeneric = document.createElement("p");
+        generalInformationGeneric.innerHTML = "Could not find any relevant information.";
+        generalInformationDiv.appendChild(generalInformationGeneric);
+    }
+    newCard.appendChild(generalInformationDiv);
+
 
     let expandableContent = createExpandableContent(data);
     newCard.appendChild(expandableContent);
@@ -99,7 +285,7 @@ function createExpandableContent(data) {
     companyContent.innerHTML = data.company;
 
     let closeDate = document.createElement("h3");
-    closeDate.innerHTML = "Application Closing Date";
+    closeDate.innerHTML = "Closing Date";
 
     let closeDateContent = document.createElement("p");
     closeDateContent.innerHTML = data.activedate.end.split('T')[0];
@@ -138,3 +324,105 @@ function createExpandableContent(data) {
     expandableCard.appendChild(summaryContent)
     return expandableCard;
 }
+
+function createFilterChips(content, type) {
+    const filtersDiv = document.getElementById("filters");
+
+    let filterBubble = document.createElement("filter-buble");
+    filterBubble.setAttribute('filter-chip-type', type)
+    filterBubble.classList.add("filter-buble");
+
+    let filterContent = document.createElement("p");
+    filterContent.innerHTML = content;
+
+    let filterCloseIcon = document.createElement("img");
+    filterCloseIcon.classList.add("filter-buble-img");
+    filterCloseIcon.src = "./Assets/images/close-icon-white.svg";
+    filterCloseIcon.style.width = "15px";
+
+    filterBubble.appendChild(filterContent);
+    filterBubble.appendChild(filterCloseIcon);
+    filtersDiv.appendChild(filterBubble);
+
+    setFilterListners();
+}
+
+function setFilterListners() {
+    const filterClose = document.querySelectorAll('.filter-buble-img');
+
+    filterClose.forEach(element => element.addEventListener('click', async (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        const closestChip = element.closest('.filter-buble');
+        const filterType = closestChip.getAttribute("filter-chip-type");
+        closestChip.remove();
+        removeFilterFromState(filterType);
+        load();
+    }));
+}
+
+function addFilterToState(filterName, data) {
+    const url = getUrl();
+    url.searchParams.append(filterName, data);
+    window.history.pushState({}, '', url);
+}
+
+function removeFilterFromState(filterName){
+    const url = getUrl();
+    url.searchParams.delete(filterName);
+    window.history.pushState({}, '', url);
+}
+
+function getFilters() {
+    return getUrl().searchParams;
+}
+
+function getUrl(){
+    return new URL(window.location.href);
+}
+
+// Page issue h
+
+async function nextPage() {
+    const header = document.getElementById("header-container");
+    header.scrollIntoView();
+
+    validatePaginationButtons(currPageSize + 10);
+
+    await GenerateData(filteredData.slice(currPageSize, currPageSize + 10));
+    currPageSize += 10;
+    header.scrollIntoView();
+}
+
+async function lastPage() {
+    const header = document.getElementById("header-container");
+    header.scrollIntoView();
+
+    validatePaginationButtons(currPageSize - 10);
+
+    await GenerateData(filteredData.slice(currPageSize - 20, currPageSize - 10));
+    currPageSize -= 10;
+}
+
+function validatePaginationButtons(value) {
+    if(value >= filteredData.length) {
+        document.getElementById("vacancies-next").style.display = "none";
+    } else {
+        document.getElementById("vacancies-next").style.display = "block";
+    }
+    
+    if(value >= 20) {
+        document.getElementById("vacancies-back").style.display = "block";
+    } else {
+        document.getElementById("vacancies-back").style.display = "none";
+    }
+}
+
+document.getElementById("vacancies-submit-button").addEventListener('click', handleSearchSubmit);
+document.getElementById("filter-button").addEventListener('click', openFilterOverlay);
+document.getElementById("filters-pop-up-close").addEventListener('click', closeFilterOverlay);
+document.getElementById("filters-submit").addEventListener('click', handleFilterSubmit);
+document.getElementById("vacancies-next").addEventListener('click', nextPage);
+document.getElementById("vacancies-back").addEventListener('click', lastPage);
+window.addEventListener("load", load);
